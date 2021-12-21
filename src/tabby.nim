dfb8aae60c866e02c658818d2c4e2cc4b6aabe5f
Andre von Houck <starplant@gmail.com>
2021-12-21 19:30:35 +0000

    Finlize out the header API.

--- src/tabby.nim
+++ src/tabby.nim
@@ -1,10 +1,11 @@
 # Fast direct CSV/TCV parser for nim.
-import strutils, parseutils
+import strutils, parseutils, strformat
 
 type TabbyError* = object of ValueError
 
 type ParseContext = ref object
   i: int
+  header: seq[string]
   data: string
   newLine: string
   sep: char
@@ -13,31 +14,64 @@ template error(msg: string, i: int) =
   ## Shortcut to raise an exception.
   raise newException(TabbyError, msg & " At offset: " & $i)
 
+proc atNewLine(p: ParseContext): bool =
+  ## Tests if the parser context is at a new line.
+  return p.data[p.i] != '\n'
+
 proc skipLine(p: ParseContext) =
+  ## Skips this line.
   let at = p.data.find(p.newLine, p.i)
   if at != -1:
     p.i = at + p.newLine.len
 
 proc skipSpaces(p: ParseContext) =
+  ## Skips spaces to next token.
   while p.i < p.data.len and p.data[p.i] == ' ':
     inc p.i
 
 proc skipSep(p: ParseContext) =
+  ## Skips current separator.
   if p.i < p.data.len and p.data[p.i] == '\n':
     return
   elif p.i < p.data.len and p.data[p.i] == p.sep:
     inc p.i
   else:
-    error("Failed to parse, seperator expected.", p.i)
+    error(&"Failed to parse, separator expected, got: {p.data[p.i]}.", p.i)
 
 proc parseHook(p: ParseContext, v: var string) =
+  ## Parse hook for string.
   p.skipSpaces()
   let start = p.i
-  while p.i < p.data.len and p.data[p.i] notin {p.sep, '\n'}:
+  if p.data[p.i] in {'"', '\''}:
+    # "quoted string"
+    let quote = p.data[p.i]
     inc p.i
-  v = p.data[start ..< p.i]
+    while p.i < p.data.len and p.data[p.i] != quote:
+      if p.data[p.i] == '\\':
+        # handle escape quote
+        inc p.i
+        let c = p.data[p.i]
+        case c:
+          of '"', '\\', '/': v.add(c)
+          of 'b': v.add '\b'
+          of 'f': v.add '\f'
+          of 'n': v.add '\n'
+          of 'r': v.add '\r'
+          of 't': v.add '\t'
+          else: v.add c
+        inc p.i
+      else:
+        v.add p.data[p.i]
+        inc p.i
+    inc p.i
+  else:
+    # plain string
+    while p.i < p.data.len and p.data[p.i] notin {p.sep, '\n'}:
+      inc p.i
+    v = p.data[start ..< p.i]
 
 proc parseHook(p: ParseContext, v: var SomeInteger) =
+  ## Parse hook for integer.
   var num: int
   let chars = parseutils.parseInt(p.data, num, p.i)
   if chars == 0:
@@ -45,53 +79,118 @@ proc parseHook(p: ParseContext, v: var SomeInteger) =
   p.i += chars
   v = num
 
+proc parseHook(p: ParseContext, v: var bool) =
+  ## Parse hook for boolean.
+  var str: string
+  p.parseHook(str)
+  v = str.toLowerAscii() == "true"
+
 proc fromCsv*[T](
   data: string,
   objType: type[seq[T]],
-  skipHeader = false, # swtich to true
-  useTab = false,
-  # newLine=Ditect,
+  header = newSeq[string](),
+  hasHeader = true,
+  useTab = false
 ): seq[T] =
   ## Read un data seq as a CSV.
+  ## * header - use this header to parse
+  ## * hasHeader - does the current data have a header,
+  ##   will be skipped if header is set.
+  ## * useTab - use tabs instead of commas.
   var p = ParseContext()
-  p.newLine = "\n"
   p.data = data
+  p.header = header
+  var userHeader = header.len != 0
+  p.newLine = "\n"
   p.sep = ','
   if useTab:
     p.sep = '\t'
 
-  if not skipHeader:
+  if hasHeader:
+    while p.atNewLine():
+      var name: string
+      p.parseHook(name)
+      if not userHeader:
+        p.header.add(name)
+      p.skipSep()
     p.skipLine()
+  else:
+    if not userHeader:
+      for name, field in T().fieldPairs:
+        p.header.add(name)
 
   while p.i < p.data.len:
     var currentRow = T()
-    for name, field in currentRow.fieldPairs:
-      parseHook(p, field)
-      p.skipSep()
+    for headerName in p.header:
+      for name, field in currentRow.fieldPairs:
+        if headerName == name:
+          parseHook(p, field)
+          p.skipSep()
     result.add(currentRow)
     p.skipLine()
 
+proc dumpHook(s: var string, v: string) =
+  var needsQuote = false
+  for c in v:
+    if c in {' ', '\t', '\n', '\r', '\\', ',', '\'', '"'}:
+      needsQuote = true
+      break
+  if needsQuote:
+    s.add '"'
+    for c in v:
+      case c:
+      of '\\': s.add r"\\"
+      of '\b': s.add r"\b"
+      of '\f': s.add r"\f"
+      of '\n': s.add r"\n"
+      of '\r': s.add r"\r"
+      of '\t': s.add r"\t"
+      of '"': s.add r"\"""
+      else:
+        s.add c
+    s.add '"'
+  else:
+    s.add v
+
+proc dumpHook[T](s: var string, v: T) =
+  s.add $v
+
 proc toCsv*[T](
   data: seq[T],
-  skipHeader = false,
+  header = newSeq[string](),
+  hasHeader = true,
   useTab = false,
 ): string =
   ## Writes out data seq as a CSV.
-  var seperator = ","
+  ## * header - use this header to write fields in specific order.
+  ## * hasHeader - should header row be written.
+  var separator = ","
   if useTab:
-    seperator = "\t"
+    separator = "\t"
 
-  if not skipHeader:
-    var headerRow = T()
-    for name, field in headerRow.fieldPairs:
-      result.add name
-      result.add seperator
-    result.removeSuffix(seperator)
+  var header = header
+  if header.len == 0:
+    for name, field in T().fieldPairs:
+      header.add(name)
+
+  if hasHeader:
+    for name in header:
+      result.dumpHook(name)
+      result.add separator
+    result.removeSuffix(separator)
     result.add("\n")
 
   for row in data:
-    for name, field in row.fieldPairs:
-      result.add $field
-      result.add seperator
-    result.removeSuffix(seperator)
+    for headerName in header:
+      var found = false
+      for name, field in row.fieldPairs:
+        if headerName == name:
+          result.dumpHook(field)
+          result.add separator
+          found = true
+          break
+      if not found:
+        result.add separator
+
+    result.removeSuffix(separator)
     result.add("\n")
--- tests/test.nim
+++ tests/test.nim
@@ -3,7 +3,8 @@
 import tabby, strutils
 
 block:
-  let csvData = """word,count
+  let csvData = """
+word,count
 the,23135851162
 of,13151942776
 and,12997637966
@@ -24,6 +25,55 @@ and,12997637966
 
   doAssert rows.toCsv() == csvData
 
+block:
+  let csvData = """
+word,count
+the,23135851162
+of,13151942776
+and,12997637966
+"""
+
+  type FreqRow = object
+    count: int
+    extra: bool
+    word: string
+
+  var rows = tabby.fromCsv(csvData, seq[FreqRow])
+  doAssert rows.len == 3
+  doAssert rows[0].word == "the"
+  doAssert rows[0].count == 23135851162
+  doAssert rows[1].word == "of"
+  doAssert rows[1].count == 13151942776
+  doAssert rows[2].word == "and"
+  doAssert rows[2].count == 12997637966
+
+  doAssert rows.toCsv(header = @["word", "count"]) == csvData
+
+block:
+  let csvData = """
+w_o_r_d,c_o_u_n_t
+the,23135851162
+of,13151942776
+and,12997637966
+"""
+
+  type FreqRow = object
+    count: int
+    extra: bool
+    word: string
+
+  var rows = tabby.fromCsv(csvData, seq[FreqRow], header = @["word", "count"])
+
+  doAssert rows.len == 3
+  doAssert rows[0].word == "the"
+  doAssert rows[0].count == 23135851162
+  doAssert rows[1].word == "of"
+  doAssert rows[1].count == 13151942776
+  doAssert rows[2].word == "and"
+  doAssert rows[2].count == 12997637966
+
+  doAssert "w_o_r_d,c_o_u_n_t\n" & rows.toCsv(
+    header = @["word", "count"], hasHeader = false) == csvData
 
 block:
   let csvData = """
@@ -36,7 +86,7 @@ and,12997637966
     word: string
     count: int
 
-  var rows = tabby.fromCsv(csvData, seq[FreqRow], skipHeader = true)
+  var rows = tabby.fromCsv(csvData, seq[FreqRow], hasHeader = false)
   doAssert rows.len == 3
   doAssert rows[0].word == "the"
   doAssert rows[0].count == 23135851162
@@ -45,7 +95,7 @@ and,12997637966
   doAssert rows[2].word == "and"
   doAssert rows[2].count == 12997637966
 
-  doAssert rows.toCsv(skipHeader = true) == csvData
+  doAssert rows.toCsv(hasHeader = false) == csvData
 
 block:
   let csvData = """
@@ -59,7 +109,9 @@ and<tab>12997637966
     word: string
     count: int
 
-  var rows = tabby.fromCsv(csvData, seq[FreqRow], useTab = true)
+  var rows = tabby.fromCsv(
+    csvData, seq[FreqRow], hasHeader = true, useTab = true
+  )
   doAssert rows.len == 3
   doAssert rows[0].word == "the"
   doAssert rows[0].count == 23135851162
@@ -69,3 +121,28 @@ and<tab>12997637966
   doAssert rows[2].count == 12997637966
 
   doAssert rows.toCsv(useTab = true) == csvData
+
+block:
+  let csvData = """
+"the apple",1
+"of,time",2
+"and\nthat",3
+"\"bye\"",4
+"""
+
+  type TextRow = object
+    text: string
+    count: int
+
+  var rows = tabby.fromCsv(csvData, seq[TextRow], hasHeader = false)
+  doAssert rows.len == 4
+  doAssert rows[0].text == "the apple"
+  doAssert rows[0].count == 1
+  doAssert rows[1].text == "of,time"
+  doAssert rows[1].count == 2
+  doAssert rows[2].text == "and\nthat"
+  doAssert rows[2].count == 3
+  doAssert rows[3].text == "\"bye\""
+  doAssert rows[3].count == 4
+
+  doAssert rows.toCsv(hasHeader = false) == csvData
